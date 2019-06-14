cf004f7031640aea5d76a9349568da9c9433fcfa
makeupyourmind <nikitosnov@gmail.com>
2019-06-14 11:36:18 +0300

    refactoring code

---
+++ config/config.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  }
+};
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 
-
 const app = express()
+
 require('dotenv').config()
 
 app.use(bodyParser.urlencoded({ extended: true }));
